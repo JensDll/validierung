9f8b95e6c58d0fb451fa7ab1b290f434657adde1
jens <jens@doellmann.com>
2022-01-07 22:28:36 +0100

    update dependencies

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,9 +1,4 @@
-import yargs from 'yargs'
-
 import { run } from './utils'
-
-const args = await yargs(process.argv.slice(2)).array('targets').argv
-args['targets'] ??= ['shared', 'core']
 
 console.log('Running rollup in watch mode ...')
 
