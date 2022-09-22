9804889f1ae09b71fd7c7bdd1d48a657d6508dd8
jens <jens@doellmann.com>
2022-09-22 17:44:06 +0200

    update playground

--- playground/vue3/tailwind.config.js
+++ playground/vue3/tailwind.config.js
@@ -19,26 +19,40 @@ module.exports = {
     extend: {}
   },
   plugins: [
-    Icons(() => ({
-      heroiconsSolid: {
-        icons: {
-          menu: {}
-        },
-        location:
-          'https://esm.sh/@iconify-json/heroicons-solid@1.1.1/icons.json'
-      },
+    Icons(({ theme }) => ({
       heroiconsOutline: {
         icons: {
-          'plus-circle': {},
-          'minus-circle': {}
+          'plus-circle': {
+            color: theme('colors.green.600'),
+            cursor: 'pointer',
+            '&:hover': {
+              color: theme('colors.green.800')
+            }
+          },
+          'minus-circle': {
+            color: theme('colors.red.600'),
+            cursor: 'pointer',
+            '&:hover': {
+              color: theme('colors.red.800')
+            }
+          }
         },
+        scale: 1.5,
         location:
-          'https://esm.sh/@iconify-json/heroicons-outline@1.1.1/icons.json'
+          'https://esm.sh/@iconify-json/heroicons-outline@1.1.4/icons.json'
       },
       custom: {
         icons: {
-          loading: {}
+          loading: {
+            '@keyframes spin': {
+              to: {
+                transform: 'rotate(360deg)'
+              }
+            },
+            animation: 'spin 600ms linear infinite'
+          }
         },
+        scale: 1.5,
         location:
           'https://gist.githubusercontent.com/JensDll/4e59cf6005f585581975941a94bc1d88/raw/0e70bdac81224add27d8f0576ab15406709e5938/icons.json'
       }
