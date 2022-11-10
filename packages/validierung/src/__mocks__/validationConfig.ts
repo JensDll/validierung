74903741a641d908734f2f4690d4481e53d10f77
jens <jens@doellmann.com>
2022-11-10 18:54:03 +0100

    remove validation config mock

--- packages/validierung/src/__mocks__/validationConfig.ts
+++
@@ -1,9 +0,0 @@
-const { ValidationConfig: ActualValidationConfig } = await vi.importActual<any>(
-  '~validierung/validationConfig'
-)
-
-class MockValidationConfig extends ActualValidationConfig {}
-
-export const ValidationConfig = vi.fn(() => new MockValidationConfig())
-
-export const validationConfig = new ValidationConfig()
